bbe9e1d7834d0e9233b0a3573bcdafca0574edbb
ckyeon <qsky8520@gmail.com>
2023-03-11 16:40:11 +0900

    iOS App Dev Tutorials: Using stacks to arrange views

---
+++ iOS App Dev Tutorials/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 조기연 on 2023/03/11.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
